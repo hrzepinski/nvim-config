df566d43c19533f828746424a02637d60b0e16f8
Hubi <hubert.rzepinski@gmail.com>
2023-05-27 21:04:07 +0200

    move multi lines

--- lua/hubi/remap.lua
+++ lua/hubi/remap.lua
@@ -4,12 +4,10 @@ vim.keymap.set("n", "<leader>w", "<C-W><C-W>")
 
 vim.keymap.set("n", "<leader>n", ":NvimTreeFocus<CR>")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<C-j>", ":m .+1<CR>==")
-vim.keymap.set("n", "<C-k>", ":m .-2<CR>==")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", ":m .+1<CR>==")
+vim.keymap.set("n", "K", ":m .-2<CR>==")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
